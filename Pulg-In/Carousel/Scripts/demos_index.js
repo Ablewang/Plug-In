eaeb31120240f87e9773d92e3742f808088c7c22
AbelWang <135161616@qq.com>
2017-06-16 11:40:30 +0800

    Plug-In
    JS插件学习之路

---
+++ Pulg-In/Carousel/Scripts/demos_index.js
@@ -0,0 +1,5 @@
+$(function(){
+	var imgs = ['Images/1.jpg','Images/2.jpg','Images/3.jpg','Images/4.jpg','Images/5.jpg'];
+	var carousel = new Carousel(document.getElementById('carousel_div'), imgs);
+	carousel.init();
+})
