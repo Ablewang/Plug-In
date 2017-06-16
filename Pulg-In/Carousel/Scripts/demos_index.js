cc6f4e64a673abaabad3040bf44bd78018f8d192
AbelWang <135161616@qq.com>
2017-06-16 11:48:20 +0800

    Delete

--- Pulg-In/Carousel/Scripts/demos_index.js
+++
@@ -1,5 +0,0 @@
-$(function(){
-	var imgs = ['Images/1.jpg','Images/2.jpg','Images/3.jpg','Images/4.jpg','Images/5.jpg'];
-	var carousel = new Carousel(document.getElementById('carousel_div'), imgs);
-	carousel.init();
-})
--- Pulg-In/Popup/Scripts/demos_index.js
+++
@@ -1,9 +0,0 @@
-var popup = null;
-$(function(){
-	popup = new Popup();
-})
-function pop(class_name){
-	if (popup) {
-		popup.msg($("."+class_name).html());
-	}
-}
